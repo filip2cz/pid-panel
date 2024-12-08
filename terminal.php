23ae3f9fdf42824dd25b9342b53339475eb87472
filip2cz <filip@fkomarek.eu>
2024-12-08 17:53:41 +0100

    ADD: odeber mezery

--- terminal.php
+++ terminal.php
@@ -34,7 +34,7 @@ $mapUrl = isset($config['mapUrl']) ? $config['mapUrl'] : 0;
             $local_ip = shell_exec("hostname -I");
 
             // Vypsání IP adresy na stránku
-            echo str_replace(["\r", "\n"], '',$local_ip);
+            echo str_replace([" "], '',str_replace(["\r", "\n"], '',$local_ip));
             ?>:8080" id="mapa" target="_self"></iframe>
 
     <div class="container">
